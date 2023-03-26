17f257000ecf8014901940a3c5c025db20abe80d
Varsha Fernandes <varsha.fernandes@asos.com>
2023-03-26 02:10:34 +0100

    Todo List Item model added

---
+++ TodoList/TodoList/Models/TodoListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
+{
+    public class TodoListItem
+    {
+        public int Id { get; set; }
+
+        public DateTime AddDate { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Title must contain at least two characters!")]
+        [MaxLength(200, ErrorMessage = "Title must contain a maximum of 200 characters!")]
+        public string Title { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
